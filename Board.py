bbe680647888a7a85724c49dd209569cd2bf004f
Vexatori <alexander.suyunov@yandex.ru>
2024-09-29 12:17:48 +0300

    changed classes dot, ship; added board class

---
+++ Board.py
@@ -0,0 +1,79 @@
+from Ship import Ship
+
+
+class Board:
+    def __init__(self):
+        self.board_cells = [
+            ['\u25EF', '\u25EF', '\u25EF', '\u25EF', '\u25EF', '\u25EF'],
+            ['\u25EF', '\u25EF', '\u25EF', '\u25EF', '\u25EF', '\u25EF'],
+            ['\u25EF', '\u25EF', '\u25EF', '\u25EF', '\u25EF', '\u25EF'],
+            ['\u25EF', '\u25EF', '\u25EF', '\u25EF', '\u25EF', '\u25EF'],
+            ['\u25EF', '\u25EF', '\u25EF', '\u25EF', '\u25EF', '\u25EF'],
+            ['\u25EF', '\u25EF', '\u25EF', '\u25EF', '\u25EF', '\u25EF']
+        ]
+        self.ships = []
+        self.__hid = True
+        self.ships_alive = 0
+
+    @property
+    def hidden(self):
+        return self.__hid
+
+    @hidden.setter
+    def hidden(self, is_hidden: bool):
+        if not isinstance(is_hidden, bool):
+            raise ValueError("Значение видимости кораблей на доске должно быть булевым")
+        self.__hid = is_hidden
+
+    def add_ship(self, ship):
+        if len(self.ships) == 7:
+            raise ValueError("На доске нет места для кораблей, уже резмещено максимальное количество")
+        if ship.length == 3 and len([s for s in self.ships if s.length == 3]) == 1:
+            raise ValueError("На доске уже есть корабль длинной в 3 клетки, вы не можете поставить еще один")
+        if ship.length == 2 and len([s for s in self.ships if s.length == 2]) == 2:
+            raise ValueError("На доске уже есть два корабля длинной в 2 клетки, вы не можете поставить больше")
+        if ship.length == 1 and len([s for s in self.ships if s.length == 2]) == 4:
+            raise ValueError("На доске уже есть четыре корабля длинной в 1 клетку, вы не можете поставить больше")
+        if not self.cells_available(ship):
+            raise ValueError("Нельзя поставить корабль на указанные клетки.")
+        if not 0 <= ship.starting_point.x <= 5 or not 0 <= ship.starting_point.y <= 5:
+            raise ValueError("Начальная точка корабля находится за пределами доски")
+        for dot in ship.dots:
+            self.board_cells[dot.x][dot.y] = '\u25A0'
+        self.ships.append((ship))
+
+    def cells_available(self, ship):
+        board_cells = []
+        ship_dots_cords = []
+        for dot in ship.dots:
+            i, j = dot.x, dot.y
+            ship_dots_cords.append(i)
+            ship_dots_cords.append(j)
+            for i in [i for i in range(i - 1, i + 2) if 0 <= i <= 5]:
+                for j in [j for j in range(j - 1, j + 2) if 0 <= j <= 5]:
+                    board_cells.append(self.board_cells[i][j])
+        return all([cell == '\u25EF' for cell in board_cells]) and all([0 <= c <= 5 for c in ship_dots_cords])
+
+    def print_board(self):
+        board_cells = []
+        for row in self.board_cells:
+            # board_cells.append(['\u25EF' if self.__hid and elem == '\u25A0' else elem for elem in row])
+            print("|".join(['\u25EF' if self.__hid and elem == '\u25A0' else elem for elem in row]))
+
+
+if __name__ == '__main__':
+    # test_board = Board()
+    # for r in test_board.board_condition:
+    #     print("|".join(r))
+    # print(list(range(-1, 2)))
+    board = Board()
+    board.hidden = False
+    ship_1 = Ship(3, (0, 0), 'v')
+    ship_2 = Ship(2, (4, 0), 'h')
+    ship_3 = Ship(2, (0, 2), 'h')
+    ship_4 = Ship(1, (0, 5), 'v')
+    board.add_ship(ship_1)
+    board.add_ship(ship_2)
+    board.add_ship(ship_3)
+    board.add_ship(ship_4)
+    board.print_board()
--- Ship.py
+++ Ship.py
@@ -1,8 +1,8 @@
-import Dot
+from Dot import Dot
 
 
 class Ship:
-    def __init__(self, length: int, dot: Dot, direction: int):
+    def __init__(self, length: int, dot: (Dot, tuple), direction: str):
         """
         Конструктор класса Ship
         Параметры
@@ -12,20 +12,21 @@ class Ship:
         dot : Dot or tuple
                 начальная точка корабля (его нос)
         direction : int
-                направление корабля: 1 - горизонтальное, 0 - вертикальное
+                направление корабля: h - горизонтальное, v - вертикальное
         """
         if not isinstance(length, int):
             raise TypeError("Длина корабля должна быть числом")
         if not isinstance(dot, Dot) and not isinstance(dot, tuple):
-            raise TypeError("Начальная точка корабля должна быть Dot")
-        if not isinstance(direction, int):
-            raise TypeError("Направление корабля должно задаваться числом")
+            raise TypeError("Начальная точка корабля должна быть точкой (Dot) или кортежем")
+        if not isinstance(direction, str) or direction not in ['h', 'v']:
+            raise TypeError("Направление корабля должно задаваться символами h - горизонтальное, или v - вертикальное")
         self.length = length
         self.health = length
-        self.starting_point = dot
+        self.starting_point = dot if isinstance(dot, Dot) else Dot(dot[0], dot[1])
         self.direction = direction
 
+    @property
     def dots(self):
         dot = self.starting_point
-        return [Dot(dot.x + (0 if self.direction == 0 else i), dot.y + (0 if self.direction == 1 else i)) for i in
+        return [Dot(dot.x + (0 if self.direction == 'h' else i), dot.y + (0 if self.direction == 'v' else i)) for i in
                 range(self.length)]
